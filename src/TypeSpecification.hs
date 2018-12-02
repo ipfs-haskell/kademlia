f9392cdb7d4a4b341b3f87348b14b6654138dbcc
adithyaov <111501017@smail.iitpkd.ac.in>
2018-12-02 15:08:50 +0530

    indented propeerly

--- src/TypeSpecification.hs
+++ src/TypeSpecification.hs
@@ -1,46 +1,55 @@
 -- Parametrize over String
 module TypeSpecification where
 
-data PingMessage = PING | PONG
+import Network.Socket (PortNumber, SockAddr(..))
 
-import Network.Socket (SockAddr(..), PortNumber, inet_ntoa, inet_addr)
-
+-- Signature of a UDP peer
 class UDPPeer a where
-	host :: a -> String
-	port :: a -> PortNumber
-	toUDPPeer :: SockAddr -> IO (Maybe a)
+  host :: a -> String
+  port :: a -> PortNumber
+  toUDPPeer :: SockAddr -> IO (Maybe a)
 
+-- Signature of an Xor distance
 class XorDistance a where
-	xor :: a -> a -> Ordering
+  xor :: a -> a -> Ordering
 
-class (XorDistance b, UDPPeer c) => Node a where
-	udpPeer :: a -> c
-	nodeId :: a -> b
+-- Signature of any Node
+class (XorDistance b, UDPPeer c) =>
+      Node a
+  where
+  udpPeer :: a -> c
+  nodeId :: a -> b
 
+-- Sort set of nodes (bucket) by their Xor distance
 sortByXor :: (Node a, XorDistance b) => [a] -> b -> [a]
-sortByXor bucket id = unpack . sort . pack $ bucket
-	where pack bk = zip bk $ map f bk
-		f = xor id . nodeId
-		sort = sortBy (compare `on` snd)
-		unzip = map fst
+sortByXor bucket id =
+  let pack bk = zip bk $ map f bk
+      f = xor id . nodeId
+      sort = sortBy (compare `on` snd)
+      unzip = map fst
+   in unpack . sort . pack $ bucket
 
+-- A Protocol signal
+data Signal i a b = Signal
+  { source :: a
+  , command :: Command i a b
+  }
 
-class (Node b, Command c) Signal a where
-	source :: a -> b
-	command :: a -> c
-
-data Command i a b =
-	-- The basic PING and PONG
-	PING
-	| PONG
-	-- Command to store a datablock `b` on a Node with id `i`
-	| STORE i b
-	-- Command to find a node with id `i`
-	| FIND_NODE i
-	-- Response to FIND_NODE
-	| RETURN_NODES i [a]
-	-- Command to find a datablock with key `i`
-	| FIND_VALUE i
-	-- Response to FIND_VALUE
-	| RETURN_VALUE i b
-
+-- Protocol Commands in IPFS
+data Command i a b
+        -- The basic PING and PONG
+  = PING
+  | PONG
+        -- Command to store a datablock `b` on a Node with id `i`
+  | STORE i
+          b
+        -- Command to find a node with id `i`
+  | FIND_NODE i
+        -- Response to FIND_NODE
+  | RETURN_NODES i
+                 [a]
+        -- Command to find a datablock with key `i`
+  | FIND_VALUE i
+        -- Response to FIND_VALUE
+  | RETURN_VALUE i
+                 b
