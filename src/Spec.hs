91bf3423ccb1fcdb553f4c8dcedfe1d76e66a57a
adithyaov <111501017@smail.iitpkd.ac.in>
2019-01-28 13:56:13 +0530

    Few errors corrected, binding missing

--- src/Spec.hs
+++ src/Spec.hs
@@ -1,44 +1,47 @@
 module Spec where
 
-import qualified Data.ByteString as BS
 import qualified Data.Bits as B
+import qualified Data.ByteString as BS
+import Data.List (sortBy)
+import Data.Function (on)
 
 -- A phantom type to represent ID
-newtype ID a = ID BS.ByteString
+newtype ID a =
+  ID BS.ByteString
 
 data NodeID
 
 -- Change this, this is the UDP peer
 data Peer = Peer
-          { peerIP :: String
-          , peerPort :: String
-          }
+  { peerIP :: String
+  , peerPort :: String
+  }
 
 -- A node in the Kademlia Network
-data Node = Node 
-          { nodeID :: ID NodeID 
-          , nodeBuckets :: [Bucket]
-          , nodePeer :: Peer
-          }
+data Node = Node
+  { nodeID :: ID NodeID
+  , nodeBuckets :: [Bucket]
+  , nodePeer :: Peer
+  }
 
 -- A bucket representation
-data Bucket = Bucket 
-            { bucketLogLower :: Integer
-            , bucketNodes :: [Node]
-            }
+data Bucket = Bucket
+  { bucketLogLower :: Integer
+  , bucketNodes :: [Node]
+  }
 
 removeIDConstructor (ID x) = x
 
+sortBucketsByDistance :: Bucket -> ID a -> Bucket
+sortBucketByDiatance bl (ID id) = unpack . sort . pack . bucketNodes $ bl
+  where
+    pack x = zip x $ map f x
+    f = unsafeXorByteString id . removeIDConstructor . nodeID
+    sort = sortBy (compare `on` snd)
+    unpack x = Bucket (bucketLogLower bl) $ map fst x
 
-sortBucketsByDistance :: [Bucket] -> ID a ->  [Bucket]
-sortBucketsByDiatance bl (ID id) = unpack . sort . pack . bucketNodes $ bl
-  where pack = zip bl $ map f bl
-        f = xorByteString id . nodeID . removeIDConstructor
-        sort = sortBy (compare `on` snd)
-        unpack = map fst
-
-xorByteString :: BS.ByteString -> BS.ByteString -> BS.ByteString 
-xorByteString a b = snd $ foldl f (b, BS.empty) a
-  where f x y = (BS.tail . fst $ x, BS.snoc (snd x) xorResult) 
-        xorResult = y `xor` BS.head . fst $ x
-
+unsafeXorByteString :: BS.ByteString -> BS.ByteString -> BS.ByteString
+unsafeXorByteString a b = snd $ BS.foldl f (b, BS.empty) a
+  where
+    f x y = (BS.tail . fst $ x, BS.snoc (snd x) $ xorResult x y)
+    xorResult x y = B.xor y $ BS.head . fst $ x
