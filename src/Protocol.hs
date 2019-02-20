3104145b5fb30d1f42afe565683662dde478fd8d
PArth__PAtel <parthp474@gmail.com>
2019-02-20 20:56:14 +0530

    Implemented findKClosestNodes fn, answer (FIND_NODE_REQUEST) modified to use it, implemented answer (FIND_VALUE_REQUEST)

--- src/Protocol.hs
+++ src/Protocol.hs
@@ -35,11 +35,98 @@ answer (STORE_REQUEST i d) =
     let nHashTable = M.insert i d . nodeHashTable $ n
     let nN = n {nodeHashTable = nHashTable}
     return (STORE_RESPONSE i, nN)
+
 answer (FIND_NODE_REQUEST id) =
   StateT $ \n -> do
-    let diff = safeXorByteString id $ nodeID n
-    let i = round . log . fromInteger . byteStringToInteger $ diff
-    return (FIND_NODE_RESPONSE $ nodeBuckets n ! i, n)
+    let kClosestNodes = findKClosestNodes n id
+    return (FIND_NODE_RESPONSE $ kClosestNodes, n)
+
+answer (FIND_VALUE_REQUEST key) = 
+  StateT $ \n -> do
+    let value = M.lookup key (nodeHashTable n)
+    case value of 
+      Just v  ->  return (FIND_VALUE_RESPONSE True v [], n)
+      Nothing ->  
+                  let
+                    kClosestNodes = findKClosestNodes n key
+                  in
+                    return (FIND_VALUE_RESPONSE False mempty kClosestNodes, n)
+
 
 nodeToTriplet :: Node -> NodeTriplet
 nodeToTriplet n = (nodeID n, nodePeer n)
+
+-- Updates Node's Buckets according to Protocol Rules.
+-- Used when Node gets a request from some client
+-- Or gets a reply from client
+refreshBucket :: Node -> NodeTriplet -> Node
+refreshBucket node clientTriplet = 
+  let
+    clientID = fst $ clientTriplet
+    nodeID_ = nodeID node
+    allBuckets = nodeBuckets node
+    bucketIndex = findBucketIndex nodeID_ clientID
+    bucket = allBuckets ! bucketIndex --TODO: handle case when bucket doesnt exist
+    --TODO: clientIndex = findIndex (\x -> (fst x) == clientID) bucket
+    clientIndex = elemIndex clientTriplet bucket
+  in
+    case clientIndex of 
+        Just i  ->  
+            let
+            --TODO: newBucket = (deleteBy (\x y-> fst x == fst y) clientTriplet kBucket) ++ [clientTriplet]
+              newBucket = moveIthToTail i bucket
+              newNodeBuckets = (//) allBuckets [(bucketIndex, newBucket)]
+            in
+              node { nodeBuckets = newNodeBuckets }
+        Nothing ->  
+            if length bucket < 4 then --TODO: use k parameter
+                node { nodeBuckets = kBuckets ++ [clientTriplet]}   
+            else
+              --TODO: Handle this case
+              if isNodeAlive (head bucket) then
+                node { nodeBuckets = (//) allBuckets 
+                          [(bucketIndex, moveIthToTail 0 bucket)] 
+                   }
+              else
+                node { nodeBuckets = (//) allBuckets 
+                          [(bucketIndex, tail bucket ++ [clientTriplet])] 
+
+
+--TODO: Use k instead of 4
+findKClosestNodes ::    Node           -- Node State
+                    ->  ID a           -- Client's ID
+                    ->  [NodeTriplet]  -- K closest Entries
+
+findKClosestNodes Node( nodeID_, buckets, _, _) id
+                
+  -- | When total no of entries <= k, returns all the entries
+  | totalEntries <= 4       = allEntries 
+  -- | When Client's corresponding bucket has k entries
+  | length mainBucket == 4  = mainBucket
+  | otherwise               = take 4 allEntriesSorted
+  
+  where
+
+    totalEntries      = sum $ map length buckets
+    allEntries        = mconcat $ toList buckets
+    bucketIndex       = findBucketIndex nodeID_ id
+    mainBucket        = buckets ! bucketIndex
+    allEntriesSorted  = S.sortBucketByDistance allEntries id
+
+
+--TODO: complete it
+isNodeAlive ::  NodeTriplet -- Triplet of Node beint Pinged
+            ->  Bool        -- Status of Ping
+isNodeAlive (id, Peer(ip, serviceName)) = True
+
+
+-- It finds index of bucket where client belongs for given Node.
+findBucketIndex ::   ID NodeID  -- Node ID
+                  -> ID NodeID  -- Client ID
+                  -> Int        -- Bucket Index
+findBucketIndex nodeID_ clientID = 
+      let
+        distance = S.safeXorByteString nodeID_ clientID
+          bucketIndex = round . logBase 2 . fromInteger . byteStringToInteger $ distance
+      in
+        bucketIndex
